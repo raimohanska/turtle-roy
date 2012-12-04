7c634bd79bdfb5acef7349d75544be38018206f8
Juha Paananen <juha.paananen@gmail.com>
2012-12-04 22:29:03 +0200

    Use TCO from https://github.com/Gozala/js-tail-call/blob/master/core.js

---
+++ src/main/webapp/lib/tco.js
@@ -0,0 +1,13 @@
+function tco(f) {
+  var result, active = false, accumulated = []
+  return function accumulator() {
+    accumulated.push(arguments)
+    if (!active) {
+      active = true
+      while (accumulated.length) result = f.apply(this, accumulated.shift())
+      active = false
+      return result
+    }
+  }
+}
+
