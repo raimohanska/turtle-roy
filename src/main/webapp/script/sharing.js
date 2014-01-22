050eeb967865e52c24e5e783d33f08070da2f329
Juha Paananen <juha.paananen@gmail.com>
2014-01-22 13:36:15 +0200

    extract save functionality to Storage

--- src/main/webapp/script/sharing.js
+++ src/main/webapp/script/sharing.js
@@ -4,44 +4,35 @@ function Sharing(code, storage) {
     $("#share form").slideToggle("fast")
   })
   var shareClick = shareButton.asEventStream("click")
-  var shared = shareClick.map(true).toProperty(false)
+  var shared = storage.saveBus.map(true).toProperty(false)
   var nickname = Bacon.$.textFieldValue($("#nick"))
   nickname.bind(storage.author)
+
   var description = Bacon.$.textFieldValue($("#description"))
   var shareData = Bacon.combineTemplate({
       author: nickname,
       description: description,
       code: code
     })
-  var shareResult = shareClick.map(shareData).map(function(data) {
-    return {
-      url: "/turtle",
-      type: "post",
-      contentType: "application/json",
-      data: JSON.stringify(data)
-    }
-  }).ajax()
-  var pending = shareClick.awaiting(shareResult)
-  pending.assign($("#share .ajax"), "toggle")
-  var changedSinceShare = code.changes().awaiting(shareClick)
+  storage.saveBus.plug(shareClick.map(shareData))
+  storage.savePending.assign($("#share .ajax"), "toggle")
+  var changedSinceShare = code.changes().awaiting(storage.saveBus)
   var okToShare = nickname.map(nonEmpty)
     .and(description.map(nonEmpty))
-    .and(pending.not())
+    .and(storage.savePending.not())
     .and(changedSinceShare)
   okToShare.not().assign(shareButton, "attr", "disabled")
   var shareLink = $("#share a")
-  var showLink = shared.and(pending.not()).and(changedSinceShare.not())
+  var showLink = shared.and(storage.savePending.not()).and(changedSinceShare.not())
   showLink.assign(shareLink, "toggle")
   showLink.not().assign(shareButton, "toggle")
-  shareResult.map(".id").onValue(function(id) {
+  storage.saveResult.map(".id").onValue(function(id) {
     shareLink.attr("href", relativeUrl(id))
     shareLink.text("share this link!")
     repl.print("Saved! URL=" + absoluteUrl(id))
   })
   var inputs = $("#share input")
-  code.changes().onValue(function() { 
-    $("#description").val("").trigger("keyup")
-  })
+  code.changes().map("")
   showLink.assign(inputs, "attr", "disabled")
   var anythingToShare = code.changes().map(true).toProperty(false)
   anythingToShare.onValue(function(val) {
--- src/main/webapp/script/storage.js
+++ src/main/webapp/script/storage.js
@@ -3,7 +3,20 @@ function Storage() {
   author.onValue(function(author) {
     $.cookie("author", author, { expires: 365 })
   })
+  var saveBus = new Bacon.Bus()
+  var saveResult = saveBus.map(function(data) {
+    return {
+      url: "/turtle",
+      type: "post",
+      contentType: "application/json",
+      data: JSON.stringify(data)
+    }
+  }).ajax()
+  var savePending = saveBus.awaiting(saveResult)
   return {
-    author: author
+    author: author,
+    saveBus: saveBus,
+    saveResult: saveResult,
+    savePending: savePending
   }
 }
