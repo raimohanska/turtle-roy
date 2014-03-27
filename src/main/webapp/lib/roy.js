f05164fa4c87a7f0332a9480955d34c91440dfd1
Juha Paananen <juha.paananen@gmail.com>
2014-03-27 21:04:41 +0200

    roy hack: allow negative numbers

--- src/main/webapp/lib/roy.js
+++ src/main/webapp/lib/roy.js
@@ -2249,9 +2249,6 @@
                 if (value !== value) {
                     throw new Error('Numeric literal whose value is NaN');
                 }
-                if (value < 0 || value === 0 && 1 / value < 0) {
-                    throw new Error('Numeric literal whose value is negative');
-                }
                 if (value === 1 / 0) {
                     return json ? 'null' : renumber ? '1e400' : '1e+400';
                 }
