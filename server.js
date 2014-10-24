7f2a660fc12a2510e746f822ca545d23de7f4a2f
Juha Paananen <juha.paananen@gmail.com>
2014-10-24 22:08:03 +0300

    add mongodb node

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var port = process.env.PORT || 3000
 var app = express()
+var MongoClient = require('mongodb').MongoClient
 
 app.use(express.compress())
 app.use(express.json())
