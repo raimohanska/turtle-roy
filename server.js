8ce0179f5649a402c7a1d304647a7ee3dcc4e363
Juha Paananen <juha.paananen@gmail.com>
2014-10-24 23:21:59 +0300

    refactor

--- server.js
+++ server.js
@@ -2,58 +2,15 @@ var express = require('express')
 var port = process.env.PORT || 3000
 var app = express()
 var MongoClient = require('mongodb').MongoClient
-var Bacon = require("baconjs")
 var url = process.env["MONGOHQ_URL"] || "mongodb://localhost/turtleroy"
+var TurtleStore = require("./turtlestore")
 
 MongoClient.connect(url, function(err, conn) {
   if (err) {
     throw err
   }
-  TurtleStore(conn, express)
+  TurtleStore(conn, app)
 })
-
-function TurtleStore(conn, express) {
-  var turtles = conn.collection("turtle")
-  var randomstring = require("randomstring")
-
-  app.get("/turtles", function(req, res) {
-    sendResult(mongoFind({}), res)
-  })
-  app.get("/turtles/:author", function(req, res) {
-    sendResult(mongoFind({"content.author": req.params.author}), res)
-  })
-  app.get("/turtle/:id", function(req, res) {
-    sendResult(mongoFind({"id": req.params.id}).map(".0"), res)
-  })
-  app.get("/turtle/:author/:name", function(req, res) {
-    sendResult(mongoFind({"content.author": req.params.author, "content.description": req.params.name}).map(".0"), res)
-  })
-  app.post("/turtle", function(req, res) {
-    var data = {
-      id: randomstring.generate(10),
-      content: req.body,
-      date: new Date()
-    }
-    sendResult(mongoPost(data).map(data), res)
-  })
-  function mongoPost(data) {
-    return Bacon.fromNodeCallback(turtles, "insert", [data])
-  }
-  function mongoFind(query) {
-    return Bacon.fromNodeCallback(turtles.find(query), "toArray")
-  }
-  function sendResult(resultE, res) {
-    resultE.log("result")
-    resultE.onError(res, "send")
-    resultE.onValue(function(value) {
-      if (value) {
-        res.json(value)
-      } else {
-        res.status(404).send("Not found")
-      }
-    })
-  }
-}
 
 app.use(express.compress())
 app.use(express.json())
---
+++ turtlestore.js
@@ -0,0 +1,46 @@
+var Bacon = require("baconjs")
+var randomstring = require("randomstring")
+
+function TurtleStore(conn, app) {
+  var turtles = conn.collection("turtle")
+
+  app.get("/turtles", function(req, res) {
+    sendResult(mongoFind({}), res)
+  })
+  app.get("/turtles/:author", function(req, res) {
+    sendResult(mongoFind({"content.author": req.params.author}), res)
+  })
+  app.get("/turtle/:id", function(req, res) {
+    sendResult(mongoFind({"id": req.params.id}).map(".0"), res)
+  })
+  app.get("/turtle/:author/:name", function(req, res) {
+    sendResult(mongoFind({"content.author": req.params.author, "content.description": req.params.name}).map(".0"), res)
+  })
+  app.post("/turtle", function(req, res) {
+    var data = {
+      id: randomstring.generate(10),
+      content: req.body,
+      date: new Date()
+    }
+    sendResult(mongoPost(data).map(data), res)
+  })
+  function mongoPost(data) {
+    return Bacon.fromNodeCallback(turtles, "insert", [data])
+  }
+  function mongoFind(query) {
+    return Bacon.fromNodeCallback(turtles.find(query), "toArray")
+  }
+  function sendResult(resultE, res) {
+    resultE.log("result")
+    resultE.onError(res, "send")
+    resultE.onValue(function(value) {
+      if (value) {
+        res.json(value)
+      } else {
+        res.status(404).send("Not found")
+      }
+    })
+  }
+}
+
+module.exports = TurtleStore
