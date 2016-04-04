304c214247710cc498cf2e81203b25c230c2068e
Juha Paananen <juha.paananen@gmail.com>
2016-04-04 23:00:26 +0300

    tweak jquery.leanmodal to close when esc is pressed

--- public/lib/jquery.leanModal.js
+++ public/lib/jquery.leanModal.js
@@ -1,64 +1,46 @@
 (function($){
+  $.fn.extend({
+    leanModal: function(options) {
+      var defaults = {
+        overlay: 0.5,
+        closeButton: null
+      };
+      var overlay = $("<div id='lean_overlay'></div>");
+      $("body").append(overlay);
+      options =  $.extend(defaults, options);
 
-    $.fn.extend({
+      return this.each(function() {
+        var o = options;
+        var modal_id = $(this).attr("href");
 
-        leanModal: function(options) {
+        $(document).keyup(function(e) {
+          // close with esc
+          if (e.keyCode == 27)  close_modal(modal_id)
+        });
 
-            var defaults = {
-                overlay: 0.5,
-                closeButton: null
-            };
+        $(this).click(function(e) {
 
-            var overlay = $("<div id='lean_overlay'></div>");
-
-            $("body").append(overlay);
-
-            options =  $.extend(defaults, options);
-
-            return this.each(function() {
-
-                var o = options;
-
-                $(this).click(function(e) {
-
-              	var modal_id = $(this).attr("href");
-
-				$("#lean_overlay").click(function() {
-                     close_modal(modal_id);
-                });
-
-                $(o.closeButton).click(function() {
-                     close_modal(modal_id);
-                });
-
-        		$('#lean_overlay').css({ 'display' : 'block', opacity : 0 });
-
-        		$('#lean_overlay').fadeTo(200,o.overlay);
-
-        		$(modal_id).css({
-
-        			'display' : 'block',
-        			'opacity' : 0,
-        			'z-index': 11000,
-        		});
-
-        		$(modal_id).fadeTo(200,1);
-
-                e.preventDefault();
-
-              	});
-
-            });
-
-			function close_modal(modal_id){
-
-        		$("#lean_overlay").fadeOut(200);
-
-        		$(modal_id).css({ 'display' : 'none' });
-
-			}
-
-        }
-    });
-
+          $("#lean_overlay").click(function() {
+            close_modal(modal_id);
+          });
+          $(o.closeButton).click(function() {
+            close_modal(modal_id);
+          });
+          $('#lean_overlay').css({ 'display' : 'block', opacity : 0 });
+          $('#lean_overlay').fadeTo(200,o.overlay);
+          $(modal_id).css({
+            'display' : 'block',
+            'opacity' : 0,
+            'z-index': 11000,
+          });
+          $(modal_id).fadeTo(200,1);
+          e.preventDefault();
+        });
+      });
+      function close_modal(modal_id){
+        $("#lean_overlay").fadeOut(200);
+        $(modal_id).css({ 'display' : 'none' });
+      }
+    }
+  });
 })(jQuery);
